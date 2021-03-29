ff6f4bbfc6ce64ec01249ad4bbf4ed34f345e763
mostafa bebo <mb4952144@gmail.com>
2021-03-29 18:04:49 +0200

    creat eng.template

---
+++ js/home.js
@@ -0,0 +1,37 @@
+// // Window.onload=function()
+// // {
+// let  
+// postes=document.querySelector(".postes"),
+// news=document.querySelector(".news"),
+// testi1=document.querySelector(".testi-1"),
+// testi2=document.querySelector(".testi-2"),
+// testi3=document.querySelector(".testi-3");;
+ 
+ 
+ 
+
+// //شغال علي صفحه الخدمات جزء our client 
+ 
+// // start gallery
+// var btns = document.getElementsByClassName("btns");
+// var current = document.getElementsByClassName("active");
+//  var attribut=document.getElement;var a=document.querySelectorAll(".a"),b=document.querySelectorAll(".b"),all=document.querySelectorAll(".all");
+// console.log(btns);
+
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//    console.log(this.getAttribute("data"));
+
+//    var img_class_belong_btb=this.getAttribute("data");
+//     if(img_class_belong_btb==="all"){ for(i=0;i<all.length;i++){all[i].style.opacity="1"}}
+//    if(img_class_belong_btb==="a"){for(i=0;i<a.length;i++){a[i].style.opacity="1"}  }
+//    if(img_class_belong_btb==="b"){if(b.classList==="b"){console.log("yes")}}
+   
+//   if (current.length >= 2) { 
+//     current[1].className = current[1].className.replace(" active", "");
+//   }
+//   this.className += " active";
+// // console.log(current.length)
+
+//   });
+// }
